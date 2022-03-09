10f5d72b1a59955838620e20d83d378dd028bfc0
Mikołaj Biel <mbiel@mailbox.org>
2022-03-10 00:18:09 +0100

    Extract ProtocolIntersectionResolver in mypy_plugin

--- typing_protocol_intersection/mypy_plugin.py
+++ typing_protocol_intersection/mypy_plugin.py
@@ -1,14 +1,13 @@
 import typing
-from typing import Optional, Callable
+from typing import Callable, Optional
 
+import mypy.errors
 import mypy.nodes
 import mypy.types
 from mypy.options import Options
-from mypy.plugin import Plugin, MethodContext, FunctionContext
-from mypy.types import Type
-import mypy.errors
+from mypy.plugin import FunctionContext, MethodContext, Plugin
 
-Context = typing.Union[MethodContext, FunctionContext]
+CallContext = typing.Union[MethodContext, FunctionContext]
 
 
 class CustomPlugin(Plugin):
@@ -16,10 +15,10 @@ class CustomPlugin(Plugin):
         super().__init__(options)
 
     def get_method_hook(self, fullname: str) -> Optional[Callable[[MethodContext], mypy.types.Type]]:
-        return intersection_method_hook
+        return intersection_function_hook
 
-    def get_function_hook(self, fullname: str) -> Optional[Callable[[FunctionContext], Type]]:
-        return intersection_method_hook
+    def get_function_hook(self, fullname: str) -> Optional[Callable[[FunctionContext], mypy.types.Type]]:
+        return intersection_function_hook
 
 
 class TypeInfoWrapper(typing.NamedTuple):
@@ -40,63 +39,64 @@ def mk_typeinfo_wrapper() -> TypeInfoWrapper:
     return TypeInfoWrapper(type_info, [])
 
 
-def _add_type_to_intersection(
-    intersection_type_info_wrapper: TypeInfoWrapper, typ: mypy.types.Instance, context: Context
-) -> None:
-    if not typ.type.is_protocol:
-        context.api.fail("Only Protocols can be used in ProtocolIntersection.", typ)
-        return
-    name_expr = mypy.nodes.NameExpr(typ.type.name)
-    name_expr.node = typ.type
-    intersection_type_info_wrapper.type_info.defn.base_type_exprs.append(name_expr)
-    intersection_type_info_wrapper.type_info.mro.append(typ.type)
-    intersection_type_info_wrapper.base_classes.append(typ.type)
+class ProtocolIntersectionResolver:
+    def __init__(self, fail: Callable[[str, mypy.nodes.Context], None]) -> None:
+        self._fail = fail
+
+    def fold_intersection(self, type_: mypy.types.Type) -> mypy.types.Type:
+        if not self._is_intersection(type_):
+            return type_
+        type_info_wrapper = self._run_fold(type_, mk_typeinfo_wrapper())
+        args = [mypy.types.Instance(ti, []) for ti in type_info_wrapper.base_classes]
+        return mypy.types.Instance(type_info_wrapper.type_info, args=args)
+
+    def _run_fold(self, type_: mypy.types.Type, intersection_type_info_wrapper: TypeInfoWrapper) -> TypeInfoWrapper:
+        if not isinstance(type_, mypy.types.Instance):
+            self._fail("Both Intersection's typevars should be a type instances!", type_)
+            return intersection_type_info_wrapper
+        if not type_.args:
+            return intersection_type_info_wrapper
+        l, r = type_.args
+        recurse_left = self._is_intersection(l)
+        if not recurse_left and isinstance(l, mypy.types.Instance):
+            self._add_type_to_intersection(intersection_type_info_wrapper, l)
+        recurse_right = self._is_intersection(r)
+        if not recurse_right and isinstance(r, mypy.types.Instance):
+            self._add_type_to_intersection(intersection_type_info_wrapper, r)
+
+        if not recurse_right and not recurse_left:
+            return intersection_type_info_wrapper
+        elif not recurse_right:
+            return self._run_fold(l, intersection_type_info_wrapper)
+        elif not recurse_left:
+            return self._run_fold(r, intersection_type_info_wrapper)
+        else:
+            return self._run_fold(l, self._run_fold(r, intersection_type_info_wrapper))
+
+    def _add_type_to_intersection(
+        self, intersection_type_info_wrapper: TypeInfoWrapper, typ: mypy.types.Instance
+    ) -> None:
+        if not typ.type.is_protocol:
+            self._fail("Only Protocols can be used in ProtocolIntersection.", typ)
+            return
+        name_expr = mypy.nodes.NameExpr(typ.type.name)
+        name_expr.node = typ.type
+        intersection_type_info_wrapper.type_info.defn.base_type_exprs.append(name_expr)
+        intersection_type_info_wrapper.type_info.mro.append(typ.type)
+        intersection_type_info_wrapper.base_classes.append(typ.type)
+
+    @staticmethod
+    def _is_intersection(typ: mypy.types.Type) -> bool:
+        return (
+            isinstance(typ, mypy.types.Instance)
+            and typ.type.fullname == "typing_protocol_intersection.types.ProtocolIntersection"
+        )
 
 
-def _fold_intersection(
-    intersection: mypy.types.Type, intersection_type_info_wrapper: TypeInfoWrapper, context: Context
-) -> TypeInfoWrapper:
-    if not isinstance(intersection, mypy.types.Instance):
-        context.api.fail("Both Intersection's typevars should be a type instances!", intersection)
-        return intersection_type_info_wrapper
-    if not intersection.args:
-        return intersection_type_info_wrapper
-    l, r = intersection.args
-    recurse_left = is_intersection(l)
-    if not recurse_left and isinstance(l, mypy.types.Instance):
-        _add_type_to_intersection(intersection_type_info_wrapper, l, context)
-    recurse_right = is_intersection(r)
-    if not recurse_right and isinstance(r, mypy.types.Instance):
-        _add_type_to_intersection(intersection_type_info_wrapper, r, context)
-
-    if not recurse_right and not recurse_left:
-        return intersection_type_info_wrapper
-    elif not recurse_right:
-        return _fold_intersection(l, intersection_type_info_wrapper, context)
-    elif not recurse_left:
-        return _fold_intersection(r, intersection_type_info_wrapper, context)
-    else:
-        return _fold_intersection(l, _fold_intersection(r, intersection_type_info_wrapper, context), context)
-
-
-def is_intersection(typ: mypy.types.Type) -> bool:
-    return (
-        isinstance(typ, mypy.types.Instance)
-        and typ.type.fullname == "typing_protocol_intersection.types.ProtocolIntersection"
-    )
-
-
-def fold_intersection(context: Context) -> mypy.types.Type:
-    t = context.default_return_type
-    type_info_wrapper = _fold_intersection(t, mk_typeinfo_wrapper(), context)
-    args = [mypy.types.Instance(ti, []) for ti in type_info_wrapper.base_classes]
-    return mypy.types.Instance(type_info_wrapper.type_info, args=args)
-
-
-def intersection_method_hook(context: Context) -> mypy.types.Type:
-    if is_intersection(context.default_return_type):
-        return fold_intersection(context)
-    return context.default_return_type
+def intersection_function_hook(context: CallContext) -> mypy.types.Type:
+    resolver = ProtocolIntersectionResolver(context.api.fail)
+    ret_type = resolver.fold_intersection(context.default_return_type)
+    return ret_type
 
 
 def plugin(version: str) -> typing.Type[Plugin]:
