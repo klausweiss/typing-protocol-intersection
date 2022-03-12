c1f2301827749040431e3c7d8ecd5e3bd267ce87
Mikołaj Biel <mbiel@mailbox.org>
2022-03-12 13:02:29 +0100

    Fix 3.7 CI, show coverage in workflow output

--- tests/test_python_module.py
+++ tests/test_python_module.py
@@ -1,4 +1,9 @@
-from typing import NamedTuple, Protocol
+from typing import NamedTuple
+
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol
 
 from typing_protocol_intersection import ProtocolIntersection
 
